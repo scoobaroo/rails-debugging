fbf7fa3b81286498541fdf1297519fb5240acd50
tgaff <tgaff@alumni.nd.edu>
2015-11-13 16:25:44 -0800

    add basic exercises

---
+++ exercises/exercise0.md
@@ -0,0 +1,21 @@
+# Pry Setup
+Let's first make sure you have pry:
+
+```bash
+gem install pry
+```
+
+Next let's setup pry to use *your favorite editor*.  
+1) open ~/.pryrc in your editor.  (create it if necessary: `touch ~/.pryrc`)
+2) add a line like below but replace `vim` with the command you use to start your favorite editor.
+```bash
+Pry.config.editor = "vim"
+```
+
+For example if you use `subl`: `Pry.config.editor = 'subl'`
+
+At this point pry should be working for you.
+
+# Check
+
+In your terminal run `pry` just like you would `irb`.  Try a few basic things you might do in `irb`.
---
+++ exercises/exercise1.rb
@@ -0,0 +1,28 @@
+# Start by opening pry and requiring this file
+#   $ pry
+#   require './exercise1.rb'
+
+# if that went well you should be able to do:
+#   dude = Dude.new
+#   dude.say_hello
+#   cow = Cow.new
+#   cow.say_hello
+
+# Exercise 1 : edit
+# Use the pry `ls` and `cd` commands to enter Cow and see it's methods
+# Use the pry edit or edit-method commands to fix Cow to be more cow-like.
+# Test your fix in pry
+# Did the original `cow` get fixed too?
+
+# Don't edit this file here!  Use pry!
+class Dude
+  def say_hello
+    puts "yo"
+  end
+end
+
+class Cow
+  def say_hello
+    puts "yo"
+  end
+end
