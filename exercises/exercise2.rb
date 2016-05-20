43df83f939e400950087ca58b8c4fee685db1a5c
Brianna Veenstra <bgveenstra@users.noreply.github.com>
2016-05-20 07:43:08 -0700

    Update exercise2.rb

--- exercises/exercise2.rb
+++ exercises/exercise2.rb
@@ -2,17 +2,17 @@
 #   $ pry
 #   require './exercise2.rb'
 
-# if that went well you should be able to do:
+# if that went well, you should be able to do:
 #   dude = Dude.new
 #   dude.say_hello
 #   cow = Cow.new
 #   cow.say_hello
 
 # Exercise 1 : edit
-# Use the pry `ls` and `cd` commands to enter Cow and see it's methods
+# Use the pry `ls` and `cd` commands to enter the Cow class and see its methods
 # Use the pry edit or edit-method commands to fix Cow to be more cow-like.
-# Test your fix in pry
-# Did the original `cow` get fixed too?
+# Test your fix in pry.
+# Did the original `cow` instance get fixed too?
 
 # Don't edit this file here!  Use pry!
 class Dude
